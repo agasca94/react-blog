42669f50466d1c1fb2475aab8005cbdcaedeb803
Arturo Gasca <agasca1994@gmail.com>
2020-01-05 14:16:00 -0600

    Handle unauthorized requests by destroying the current session

--- src/index.js
+++ src/index.js
@@ -2,9 +2,23 @@ import ReactDOM from 'react-dom';
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import store from './store'
+import { client } from './api/client';
+import store from './store';
+import { signOut } from './actions/auth';
 
 import App from './App';
+
+const { dispatch } = store;
+client.interceptors.response.use(
+    response => response,
+    error => {
+        const { status } = error.response;
+        if (status === 401) {
+            dispatch(signOut());
+        }
+        return Promise.reject(error);
+    }
+);
 
 ReactDOM.render((
     <Provider store={store}>
